480abe89eb9eef6bc5dd6920bdaf29b6c7966984
Nivition <nbbourgoin@gmail.com>
2014-12-30 12:23:40 +0100

    Maj

--- GestionFichierCours/src/com/itparis/b3/poo/dao/CoursDAO.java
+++ GestionFichierCours/src/com/itparis/b3/poo/dao/CoursDAO.java
@@ -3,6 +3,7 @@ package com.itparis.b3.poo.dao;
 
 import java.sql.SQLException;
 
+import com.itparis.b3.poo.beans.Classe;
 import com.itparis.b3.poo.beans.Cours;
 import com.itparis.b3.poo.commons.DBAction;
 
@@ -26,15 +27,15 @@ public class CoursDAO {
 		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
 		CoursTemp.setNomCours(DBAction.getRes().getString(2));
 		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
+		CoursTemp.setIdCours(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(5));
 		
 
 		// Fermeture de la connexion
 		DBAction.DBClose();
 		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
+		return CoursTemp;
 	}
 	
 	public static Cours getCoursByNomCours(String nomCours) throws SQLException {
@@ -43,17 +44,16 @@ public class CoursDAO {
 		DBAction.DBConnexion();
 		DBAction.setRes(DBAction.getStm().executeQuery(req));
 		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
-		CoursTemp.setdNomCours(DBAction.getRes().getString(2));
+		CoursTemp.setNomCours(DBAction.getRes().getString(2));
 		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
-		
+		CoursTemp.setIdCours(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(5));
 
 		// Fermeture de la connexion
 		DBAction.DBClose();
 		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
+		return CoursTemp;
 	}
 	
 	public static Cours getCoursByDateCours(String dateCours) throws SQLException {
@@ -62,17 +62,16 @@ public class CoursDAO {
 		DBAction.DBConnexion();
 		DBAction.setRes(DBAction.getStm().executeQuery(req));
 		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
-		CoursTemp.setdNomCours(DBAction.getRes().getString(2));
+		CoursTemp.setNomCours(DBAction.getRes().getString(2));
 		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
-		
+		CoursTemp.setIdCours(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(5));
 
 		// Fermeture de la connexion
 		DBAction.DBClose();
 		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
+		return CoursTemp;
 	}
 	
 	
@@ -82,19 +81,36 @@ public class CoursDAO {
 		DBAction.DBConnexion();
 		DBAction.setRes(DBAction.getStm().executeQuery(req));
 		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
-		CoursTemp.setdNomCours(DBAction.getRes().getString(2));
+		CoursTemp.setNomCours(DBAction.getRes().getString(2));
 		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
-		
+		CoursTemp.setIdCours(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(5));
 
 		// Fermeture de la connexion
 		DBAction.DBClose();
 		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
+		return CoursTemp;
 	}
 	
+	
+	public static Cours getCoursByIdMatiere(int idMatiere) throws SQLException {
+		Cours CoursTemp = new Cours(0,"","",0,0,0);
+		String req = "SELECT * FROM Cours WHERE IdModule = \"" +idMatiere+"\"";
+		DBAction.DBConnexion();
+		DBAction.setRes(DBAction.getStm().executeQuery(req));
+		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
+		CoursTemp.setNomCours(DBAction.getRes().getString(2));
+		CoursTemp.setDateCours(DBAction.getRes().getString(3));
+		CoursTemp.setIdCours(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(5));
+
+		// Fermeture de la connexion
+		DBAction.DBClose();
+		// Retourner l'objet ChambreTemps
+		return CoursTemp;
+	}
 	
 	public static Cours getCoursByIdModule(int idModule) throws SQLException {
 		Cours CoursTemp = new Cours(0,"","",0,0,0);
@@ -102,55 +118,17 @@ public class CoursDAO {
 		DBAction.DBConnexion();
 		DBAction.setRes(DBAction.getStm().executeQuery(req));
 		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
-		CoursTemp.setdNomCours(DBAction.getRes().getString(2));
+		CoursTemp.setNomCours(DBAction.getRes().getString(2));
 		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
+		CoursTemp.setIdCours(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(5));
 		
 
 		// Fermeture de la connexion
 		DBAction.DBClose();
 		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
-	}
-	
-	public static Cours getCoursById(int id) throws SQLException {
-		Cours CoursTemp = new Cours(0,"","",0,0,0);
-		String req = "SELECT * FROM Cours WHERE Id = \"" +id+"\"";
-		DBAction.DBConnexion();
-		DBAction.setRes(DBAction.getStm().executeQuery(req));
-		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
-		CoursTemp.setdNomCours(DBAction.getRes().getString(2));
-		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
-		
-
-		// Fermeture de la connexion
-		DBAction.DBClose();
-		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
-	}
-	
-	public static Cours getCoursByNomCours(String nomCours) throws SQLException {
-		Cours CoursTemp = new Cours(0,"","",0,0,0);
-		String req = "SELECT * FROM Cours WHERE NomCours = \"" +nomCours+"\"";
-		DBAction.DBConnexion();
-		DBAction.setRes(DBAction.getStm().executeQuery(req));
-		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
-		CoursTemp.setdNomCours(DBAction.getRes().getString(2));
-		CoursTemp.setDateCours(DBAction.getRes().getString(3));
-		CoursTemp.setIdCours(DBAction.getRes().getString(4));
-		CoursTemp.setIdMatiere(DBAction.getRes().getString(5));
-		CoursTemp.setIdModule(DBAction.getRes().getString(5));
-		
-
-		// Fermeture de la connexion
-		DBAction.DBClose();
-		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
+		return CoursTemp;
 	}
 	
 	
@@ -162,11 +140,12 @@ public class CoursDAO {
 	
 	
 	
-	public static int deleteClasseByNomClasse(String nomClasse) {
+	
+	public static int deleteClasseById(int Id) {
 		int result = -1;
 		DBAction.DBConnexion();
 
-		String req = "DELETE FROM Classe  WHERE NomClasse =" + nomClasse;
+		String req = "DELETE FROM Classe  WHERE Id =" + Id;
 		try {
 			result = DBAction.getStm().executeUpdate(req);
 			System.out.println("Requete executee");
@@ -178,11 +157,75 @@ public class CoursDAO {
 		return result;
 	}
 	
-	public static int deleteClasseByIdClasse(int id) {
+	public static int deleteClasseByNomCours(String nomCours) {
 		int result = -1;
 		DBAction.DBConnexion();
 
-		String req = "DELETE FROM Classe  WHERE id =" + id;
+		String req = "DELETE FROM Classe  WHERE NomCours =" + nomCours;
+		try {
+			result = DBAction.getStm().executeUpdate(req);
+			System.out.println("Requete executee");
+			
+		} catch (SQLException ex) {
+			result = - ex.getErrorCode();
+		}
+		DBAction.DBClose();
+		return result;
+	}
+	
+	public static int deleteClasseByDateCours(String DateCours) {
+		int result = -1;
+		DBAction.DBConnexion();
+
+		String req = "DELETE FROM Classe  WHERE DateCours =" + DateCours;
+		try {
+			result = DBAction.getStm().executeUpdate(req);
+			System.out.println("Requete executee");
+			
+		} catch (SQLException ex) {
+			result = - ex.getErrorCode();
+		}
+		DBAction.DBClose();
+		return result;
+	}
+	
+	public static int deleteClasseByIdProf(int IdProf) {
+		int result = -1;
+		DBAction.DBConnexion();
+
+		String req = "DELETE FROM Classe  WHERE IdProf =" + IdProf;
+		try {
+			result = DBAction.getStm().executeUpdate(req);
+			System.out.println("Requete executee");
+			
+		} catch (SQLException ex) {
+			result = - ex.getErrorCode();
+		}
+		DBAction.DBClose();
+		return result;
+	}
+	
+	public static int deleteClasseByIdMatiere(int IdMatiere) {
+		int result = -1;
+		DBAction.DBConnexion();
+
+		String req = "DELETE FROM Classe  WHERE IdMatière =" + IdMatiere;
+		try {
+			result = DBAction.getStm().executeUpdate(req);
+			System.out.println("Requete executee");
+			
+		} catch (SQLException ex) {
+			result = - ex.getErrorCode();
+		}
+		DBAction.DBClose();
+		return result;
+	}
+	
+	public static int deleteClasseByIdModule(int IdModule) {
+		int result = -1;
+		DBAction.DBConnexion();
+
+		String req = "DELETE FROM Classe  WHERE IdMatière =" + IdModule;
 		try {
 			result = DBAction.getStm().executeUpdate(req);
 			System.out.println("Requete executee");
@@ -195,19 +238,27 @@ public class CoursDAO {
 	}
 	
 	
-	public static Classe updateClasseById(int id,String nom) throws SQLException {
-		Classe ClasseTemp = new Classe(0,"");
-		String req = "UPDATE Classe SET NomCLasse = \"" + nom+"\" WHERE IdClasse = \"" + id+"\"";
+	
+	
+	
+	public static Cours updateCoursById(int idCours,String nomCours,String dateCours, int idProf, int idMatiere, int idModule) throws SQLException {
+		Cours CoursTemp = new Cours(idCours,nomCours, dateCours, idProf, idMatiere, idModule);
+		
+		String req = "UPDATE Cours SET NomCLasse = \"" + nomCours+"\", DateCours = \"" + dateCours+"\", IdCLasse = \"" + idProf +"\", idMatiere = \"" + idMatiere+"\", idModule = \"" + idModule+"\" WHERE IdCours = \"" + idCours+"\"";
 		DBAction.DBConnexion();
 		DBAction.setRes(DBAction.getStm().executeQuery(req));
-		ClasseTemp.setNo(DBAction.getRes().getInt(1));
-		ClasseTemp.setNum(DBAction.getRes().getInt(2));
-		ClasseTemp.setPrix(DBAction.getRes().getDouble(3));
-		
+		CoursTemp.setIdCours(DBAction.getRes().getInt(1));
+		CoursTemp.setNomCours(DBAction.getRes().getString(2));
+		CoursTemp.setDateCours(DBAction.getRes().getString(3));
+		CoursTemp.setIdProf(DBAction.getRes().getInt(4));
+		CoursTemp.setIdMatiere(DBAction.getRes().getInt(5));
+		CoursTemp.setIdModule(DBAction.getRes().getInt(4));
 
 		// Fermeture de la connexion
 		DBAction.DBClose();
 		// Retourner l'objet ChambreTemps
-		return ClasseTemp;
+		return CoursTemp;
 	}
+	
+	
 }
--- GestionFichierCours/src/com/itparis/b3/poo/dao/UserDAO.java
+++ GestionFichierCours/src/com/itparis/b3/poo/dao/UserDAO.java
@@ -3,7 +3,7 @@ package com.itparis.b3.poo.dao;
 public class UserDAO {
 
 	public UserDAO() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
